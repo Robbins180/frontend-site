6fde90db34717944a1393471554ea6f245007109
Drew R <drewr@Drews-MacBook-Pro.local>
2021-08-04 18:45:31 -0500

    5c

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,23 +1,21 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Cards.css';
 
 function CardItem(props) {
   return (
     <>
-      <li className='cards_item'>
-        <Link className='cards_item_link' to={props.path}>
-          <figure
-          className='cards_item_pic-wrap'
-          data-catagory={props.label}>
+      <li className='cards__item'>
+        <Link className='cards__item__link' to={props.path}>
+          <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img
-            src={props.src}
-            alt='Travel Image'
-            className='cards_item_img'
+              className='cards__item__img'
+              alt='Travel Image'
+              src={props.src}
             />
           </figure>
-          <div className='cards_item_info'>
-            <h5 className='cards_item_text'>{props.text}</h5>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__text'>{props.text}</h5>
           </div>
         </Link>
       </li>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,27 +1,51 @@
 import React from 'react';
-import CardItem from './CardItem'
 import './Cards.css';
+import CardItem from './CardItem';
 
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these Epic Cards</h1>
-      <div className='cards_container'>
-        <div className='cards_wrapper'>
-          <ul className='cards_items'>
-            <CardItem src='/images/Cover.jpeg'
-            text='Creative images displayed'
-            label='Abstract'
-            path='/services'/>
-            <CardItem src='/images/Cover.jpeg'
-            text='Creative images displayed'
-            label='Abstract'
-            path='/services'/>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='/images/Cover.jpeg'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src='/images/Cover.jpeg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='/images/Cover.jpeg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='/images/Cover.jpeg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='/images/Cover.jpeg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
           </ul>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Cards;
